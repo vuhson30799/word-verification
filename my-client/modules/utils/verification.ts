95b40a006ec9d2e1cc1e53e064e40b0f1ac1bf4e
Son Hoang Vu <son.hoang.vu@mgm-tp.com>
2022-08-11 23:30:12 +0700

    WV-53: Escape special character from verification.

--- my-client/modules/utils/verification.ts
+++ my-client/modules/utils/verification.ts
@@ -1,18 +1,21 @@
 import {decoding, emptyString, encoding} from "../../constant/ApplicationConstant";
 
 export function verifyAnswer(studentAnswer: string, keys: string[]): boolean {
-    const encodedAnswer = Buffer.from(studentAnswer.toLowerCase()).toString(encoding)
     const decodedKeys = keys.map((key) => {
         const decodedKey = Buffer.from(key, encoding)
-            .toString(decoding)
-        return decodedKey.replace(`${process.env.NEXT_PUBLIC_ENCODING_KEY}`, emptyString)
+            .toString(decoding).replace(`${process.env.NEXT_PUBLIC_ENCODING_KEY}`, emptyString)
+        return Buffer.from(decodedKey, encoding).toString(decoding)
     })
 
-    return decodedKeys.some(key => key === encodedAnswer)
+    return decodedKeys.some(key => escapeSpecialCharacter(key) === escapeSpecialCharacter(studentAnswer.toLowerCase()))
 }
 
 export function translateKey(key: string): string {
     const decodedKey = Buffer.from(key, encoding).toString(decoding)
     return Buffer.from(decodedKey.replace(`${process.env.NEXT_PUBLIC_ENCODING_KEY}`, emptyString), encoding)
         .toString(decoding)
+}
+
+function escapeSpecialCharacter(input: string): string {
+    return input.replace(/[^a-zA-Z ]/g, "")
 }
