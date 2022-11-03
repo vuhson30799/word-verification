633c68b1ec94089cb6768c53f89e25adec594271
Son Hoang Vu <son.hoang.vu@mgm-tp.com>
2022-11-03 20:06:36 +0100

    WV-57: Deploy app to heroku

--- backend/src/main/java/com/english/word/verification/backend/repository/ExaminationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.english.word.verification.backend.repository;
-
-import com.english.word.verification.backend.entity.Examination;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ExaminationRepository extends MongoRepository<Examination, String> {
-}
