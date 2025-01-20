1b57e3c6553f934960801cbb19865a2b4c8f7b9c
Hoang Son <vuhson30799@gmail.com>
2025-01-20 11:02:31 +0700

    WV-76: Make dashboard empty.

--- my-client/pages/index.tsx
+++ my-client/pages/index.tsx
@@ -1,26 +1,21 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Word Verification</title>
-                <meta name="description" content="Created by SHVU"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Word Verification</title>
+        <meta name="description" content="Created by"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://www.facebook.com/phuongliendn0304">Miss Lien&lsquo;s English class!</a>
-                </h1>
-                <Image src="/lich_hoc.jpeg" alt="Lich hoc" width={500} height={500}/>
-            </main>
-        </div>
-    )
+      <main className={styles.main}>
+      </main>
+    </div>
+  )
 }
 
 export default Home
